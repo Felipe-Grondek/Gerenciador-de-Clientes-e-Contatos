d6d0e6edb11ff2dd9f25b29a75a413a68ccb7ef3
FelipeGK <felipegrondek00@gmail.com>
2023-03-31 10:28:51 -0300

    fix: add vercel config to deploy

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,15 +3,8 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-(async () => {
-    await prisma.$connect()
-    .catch((err: any) => {
-        console.log("Error during Prisma initialization", err)
-    })
-
     app.listen(3001, () => {
         console.log("Server running on http://localhost:3001")
     })
-})()
 
 export { prisma };
