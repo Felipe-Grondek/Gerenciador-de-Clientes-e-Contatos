0908eebc0ef5c053b33bb09a5ffb1141d17ce046
FelipeGK <felipegrondek00@gmail.com>
2023-03-31 10:37:20 -0300

    fix: add client base url link

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -3,7 +3,7 @@ import axios from "axios"
 const token = localStorage.getItem("@gerenciador:token")
 
 export const instance = axios.create({
-    baseURL: "http://localhost:3001/",
+    baseURL: "https://gerenciador-de-clientes-e-contatos.vercel.app/",
     headers: {
         "Content-Type": "application/json",
         Authorization: `Bearer ${token}`
